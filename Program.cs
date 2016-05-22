7476d77a7978eb124d048282b419a42a1a39c0d4
Cranberries <yussa.de.dannann@gmail.com>
2016-05-23 00:38:31 +0900

    拡張メソッドやめた、Intervalをstructにした、他

--- Program.cs
+++ Program.cs
@@ -2,13 +2,11 @@
 
 namespace cranberries
 {
-	public class Interval
-	{
-		public double Lower { get; private set; } = 0;
-		public double Upper { get; private set; } = 0;
+	public struct Interval
 
-		public Interval()
-			: this(new double(), new double()) { }
+	{
+		private double Lower;
+		private double Upper;
 
 		public Interval(double l, double u)
 		{
@@ -113,16 +111,18 @@ namespace cranberries
 			}
 		}
 
-		public override bool Equals(object obj)
-		{
-			Interval p = obj as Interval;
-			if ((object)p == null)
-			{
-				return false;
-			}
+		public override bool Equals(object obj) => base.Equals(obj);
 
-			return base.Equals(obj) && Lower == p.Lower && Upper == p.Upper;
+		public bool Equals(Interval x)
+		{
+			// If parameter is null return false:
+			if ((object)x == null) return false;
+
+			// Return true if the fields match:
+			return (Lower == x.Lower) && (Upper == x.Upper);
 		}
+
+		public override int GetHashCode() => base.GetHashCode();
 
 		public static bool operator !=(Interval l, Interval r) => !(l.Lower == r.Lower && l.Upper == r.Upper);
 
@@ -136,8 +136,6 @@ namespace cranberries
 
 		public static bool operator >=(Interval l, Interval r) => r.Upper <= l.Lower;
 
-		public override int GetHashCode() => base.GetHashCode();
-
 		public bool IsSingleton => Lower == Upper;
 
 		public double Width => Upper - Lower;
@@ -146,18 +144,15 @@ namespace cranberries
 
 		public double Rad => (Upper - Lower) / 2.0;
 
-		public new string ToString => $"[{Lower}, {Upper}]";
+		public override string ToString() => $"[{Lower}, {Upper}]";
 
 		// Constants
 		public Interval PI => new Interval(Math.PI, Math.PI);
-	}
 
-	public static class Mi
-	{
-		public static Interval Sin(this Interval x)
+		public Interval Sin()
 		{
-			var a = x.Lower;
-			var b = x.Upper;
+			var a = Lower;
+			var b = Upper;
 
 			if (b - a >= 2.0 * Math.PI)
 			{
@@ -207,21 +202,20 @@ namespace cranberries
 			}
 		}
 
-		public static Interval Cos(this Interval x)
+		public Interval Cos()
 		{
-			var a = x.Lower;
-			var b = x.Upper;
+			var a = Lower;
+			var b = Upper;
 
 			if (b - a >= 2.0 * Math.PI)
 			{
 				return new Interval(-1, 1);
 			}
 			/*  base point reset  */
-
 			var base1 = Math.Ceiling(a / (2.0 * Math.PI));
 			var base2 = Math.Ceiling(a / (2.0 * Math.PI) - 0.5);
 			var x1 = a / (2.0 * Math.PI);
-			var y1 = a / (2.0 * Math.PI) - 0.5;
+			var y1 = a / (2.0 * Math.PI) - 0.5;
 			var x2 = b / (2.0 * Math.PI);
 			var y2 = b / (2.0 * Math.PI) - 0.5;
 			/*  checking phase  */
@@ -232,14 +226,14 @@ namespace cranberries
 					return new Interval(-1, 1);
 				}
 				else
-				{
+				{
 					var l = Math.Cos(a);
 					var r = Math.Cos(b);
 					return new Interval(Math.Min(l, r), 1);
 				}
 			}
 			else if (y1 < base2 && base2 < y2)
-			{
+			{
 				var l = Math.Cos(a);
 				var r = Math.Cos(b);
 				return new Interval(-1, Math.Max(l, r));
@@ -247,79 +241,61 @@ namespace cranberries
 			else
 			{
 				if (Math.Cos(a) < Math.Cos(b))
-				{
-					var l = Math.Cos(a);
+				{
+					var l = Math.Cos(a);
+
 					var r = Math.Cos(b);
 					return new Interval(l, r);
 				}
 				else
-				{
-					var l = Math.Cos(b);
+				{
+					var l = Math.Cos(b);
+
 					var r = Math.Cos(a);
 					return new Interval(l, r);
 				}
 			}
 		}
 
-		public static Interval Tan(this Interval x)
+		public Interval Tan()
 		{
-			if (Math.Ceiling(x.Upper * 2.0 / Math.PI) - Math.Ceiling(x.Lower * 2.0 / Math.PI) != 0) throw new OverflowException();
-			return new Interval(Math.Tan(x.Lower), Math.Tan(x.Upper));
+			if (Math.Ceiling(Upper * 2.0 / Math.PI) - Math.Ceiling(Lower * 2.0 / Math.PI) != 0) throw new OverflowException();
+			return new Interval(Math.Tan(Lower), Math.Tan(Upper));
 		}
 
-		public static Interval Asin(this Interval x)
+		public Interval Asin()
 		{
-			if (x.Lower < -1.0 || 1.0 < x.Upper) throw new ArgumentOutOfRangeException();
-			return new Interval(Math.Asin(x.Lower), Math.Asin(x.Upper));
+			if (Lower < -1.0 || 1.0 < Upper) throw new ArgumentOutOfRangeException();
+			return new Interval(Math.Asin(Lower), Math.Asin(Upper));
 		}
 
-		public static Interval Acos(this Interval x)
+		public Interval Acos()
 		{
-			if (x.Lower < -1.0 || 1.0 < x.Upper) throw new ArgumentOutOfRangeException();
-			return new Interval(Math.Acos(x.Upper), Math.Acos(x.Lower));
+			if (Lower < -1.0 || 1.0 < Upper) throw new ArgumentOutOfRangeException();
+			return new Interval(Math.Acos(Upper), Math.Acos(Lower));
 		}
-
-		public static Interval Atan(this Interval x) => new Interval(Math.Atan(x.Lower), Math.Atan(x.Upper));
-
-		public static Interval Sinh(this Interval x) => new Interval(Math.Sinh(x.Lower), Math.Sinh(x.Upper));
-
-		public static Interval Cosh(this Interval x)
-		{
-			if (x.Upper < 0.0) return new Interval(Math.Cosh(x.Upper), Math.Cosh(x.Lower));
-			else if (0.0 < x.Lower) return new Interval(Math.Cosh(x.Lower), Math.Cosh(x.Upper));
-			else return new Interval(1, Math.Max(Math.Cosh(x.Lower), Math.Cosh(x.Upper)));
-		}
-
-		public static string ToString(this Interval x) => x.ToString;
-
-		public static double Width(this Interval x) => x.Width;
-
-		public static double Middle(this Interval x) => x.Middle;
-
-		public static double Lower(this Interval x) => x.Lower;
-
-		public static double Upper(this Interval x) => x.Upper;
-
-		public static bool IsSigleton(this Interval x) => x.IsSingleton;
 	}
 
 	internal class Program
 	{
 		private static void Main(string[] args)
 		{
+			var zero = new Interval();
 			var x = new Interval(-1, 2);
 			var y = new Interval(1, 2);
 			var z = new Interval(1, 2);
-			Console.WriteLine($"{nameof(x)} = {x.ToString}");
-			Console.WriteLine($"{nameof(y)} = {y.ToString}");
+			Console.WriteLine($"zero = {zero.ToString()}");
+			Console.WriteLine($"{nameof(x)} = {x.ToString()}");
+			Console.WriteLine($"{nameof(y)} = {y.ToString()}");
 
-			Console.WriteLine($"x + y = {Mi.ToString(x + y)}");
-			Console.WriteLine($"x + 1 = {Mi.ToString(x + 1)}");
-			Console.WriteLine($"x - y = {Mi.ToString(x - y)}");
-			Console.WriteLine($"x * y = {Mi.ToString(x * y)}");
-			Console.WriteLine($"x / y = {Mi.ToString(x / y)}");
+			Console.WriteLine($"x + y = {(x + y).ToString()}");
+			Console.WriteLine($"x + 1 = {(x + 1).ToString()}");
+			Console.WriteLine($"x - y = {(x - y).ToString()}");
+			Console.WriteLine($"x * y = {(x * y).ToString()}");
+			Console.WriteLine($"x / y = {(x / y).ToString()}");
+			Console.WriteLine($"y.Equals(z) = {y.Equals(z)}");
 			Console.WriteLine($"y == z -> {y == z}");
-			Console.WriteLine($"Sin(x) = {x.Sin().ToString}");
+			Console.WriteLine($"Sin(x) = {x.Sin().ToString()}");
 			Console.Read();
 		}
 	}
